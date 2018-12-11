75b84aaab9c97b6d02f4156a0788cfd15d424aa7
Joe Maples <joe@frap129.org>
2018-12-11 08:50:47 -0500

    Add Exam Mode to show work
    Signed-off-by: Joe Maples <joe@frap129.org>

--- multiplyPoint.py
+++ multiplyPoint.py
@@ -2,20 +2,35 @@ from pointUtils import add
 from pointUtils import double
 from pointUtils import getSequence
 from pointUtils import printMultiple
+from pointUtils import EXAM_MODE
+
+def printAdd(currentPoint, pDiff):
+    print str(pDiff)+"P + 1P = (" + str(currentPoint[0]) + ", " + str(currentPoint[1]) + ")"
 
 # Needed values
-multiple = 15
-point = (8, 10)
-prime = 29
-a = 4
+multiple = 13
+point = (5, 9)
+prime = 11
+a = 1
+
+
+# For showing work
+currentMultiple = 1
 
 currentPoint = point
 for x in getSequence(multiple):
     if (int(x) == 1):
         currentPoint = double(point, prime, a)
+        if (EXAM_MODE):
+            currentMultiple = currentMultiple * 2
+            printMultiple(currentPoint, currentMultiple)
         currentPoint = add(point, currentPoint, prime)
+        if (EXAM_MODE):
+            printAdd(currentPoint, currentMultiple)
+            currentMultiple = currentMultiple + 1
     else:
         currentPoint = double(currentPoint, prime, a)
-
+        if (EXAM_MODE):
+            printMultiple(currentPoint, currentMultiple*2)
 
 printMultiple(currentPoint, multiple)
--- pointUtils.py
+++ pointUtils.py
@@ -1,5 +1,7 @@
 from gmpy2 import invert
 
+EXAM_MODE = False # Set to true to show work
+
 # Special mod function to make sure x mod x = x, not x mod x = 0
 def mod(int, p):
     if (int == p):
@@ -32,7 +34,13 @@ def sValue(num, den, prime):
 # Calculate a point addition
 def newPoint(pointOne, pointTwo, prime, s):
     x = mod((((s**2) - pointOne[0]) - pointTwo[0]), prime)
+    if (EXAM_MODE):
+        print "x = "+str(s)+"^2 - "+str(pointOne[0])+" - "+str(pointOne[1])+" mod "+str(prime)
+        print "x = "+str(x)
     y = mod(((s * (pointOne[0] - x)) - pointOne[1]), prime)
+    if (EXAM_MODE):
+        print "y = "+str(s)+"("+str(pointOne[0])+" - "+str(x)+") - "+str(pointOne[1])+" mod "+str(prime)
+        print "y = "+str(y)
     return (x, y)
 
 # Calculate s value for doubling and double
@@ -41,7 +49,10 @@ def double(point, prime, a):
     sn = (3 * (point[0]**2)) + a
     sd = 2 * point[1]
     s = sValue(sn, sd, prime)
-
+    if (EXAM_MODE):
+        print "S = (3 * "+str(point[0])+"^2 + "+str(a)+")/(2 * "+str(point[1])+") mod "+str(prime)
+        print "S = "+str(sn)+"/"+str(sd)+" mod "+str(prime)
+        print "S = "+str(s)
 
     # If s > prime, must be an infinite point
     if (s > prime):
@@ -56,6 +67,10 @@ def add(pointOne, pointTwo, prime):
     sn = (pointTwo[1] - pointOne[1])
     sd = (pointTwo[0] - pointOne[0])
     s = sValue(sn, sd, prime)
+    if (EXAM_MODE):
+        print "S = ("+str(pointTwo[1])+" - "+str(pointOne[1])+")/("+str(pointTwo[0])+" - "+str(pointOne[1])+") mod "+str(prime)
+        print "S = "+str(sn)+"/"+str(sd)+" mod "+str(prime)
+        print "S = "+str(s)
 
     # If s > prime, must be an infinite point
     if (s > prime):
@@ -67,6 +82,8 @@ def add(pointOne, pointTwo, prime):
 # Converts int to binary, removes leading '0b' and the greatest bit
 # Returns sequence as a list
 def getSequence(multiple):
+    if (EXAM_MODE):
+        print "Double and Add sequence: " + str(bin(multiple)[2:])
     return list(bin(multiple)[3:])
 
 def printMultiple(currentPoint, multiple):
